e6c4cf0adc060e94f68b52c86a9542d91bcd1208
dendi-toped <dendi.sunardi@tokopedia.com>
2017-09-10 19:33:09 +0700

    modify navbar style

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,19 +6,23 @@ import './Navbar.scss'
 class Navbar extends Component {
   render () {
     return (
-      <nav className='navbar container'>
-        <div className='navbar-brand'>
-          <Link to='/' className='navbar-item' >
-            <img src={Logo} alt='mooda' width='112' height='28' />
-          </Link>
-          <div className='navbar-burger burger' data-target='navMenuExample'>
-            <span />
-            <span />
-            <span />
+      <nav className='navbar navbar-active'>
+        <div className='container'>
+          <div className='navbar-brand'>
+            <Link to='/' className='navbar-item' >
+              <img src={Logo} alt='mooda' width='112' height='28' />
+            </Link>
+            <div className='navbar-burger burger' data-target='navMenuExample'>
+              <span />
+              <span />
+              <span />
+            </div>
           </div>
+          {
+          /* <div id='navMenuExample' className='navbar-menu'>
+        </div> */
+          }
         </div>
-        {/* <div id='navMenuExample' className='navbar-menu'>
-        </div> */}
       </nav>
     )
   }
